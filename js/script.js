6ee37d1f02572ecd43f37fd7ed7b50e324e8d1d9
cant1kk <91024197+cant1kk@users.noreply.github.com>
2022-12-27 18:05:57 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function serializeForm(formNode) {
+    const { elements } = formNode
+    const data = Array.from(elements)
+      .filter((item) => !!item.name)
+      .map((element) => {
+        const { name, value } = element
+  
+        return { name, value }
+      })
+  
+    console.log(data)
+  }
+  
+  
+  
+  function handleFormSubmit(event) {
+    event.preventDefault()
+    serializeForm(applicantForm)
+  }
+  
+  const applicantForm = document.getElementById('contact-form')
+  applicantForm.addEventListener('submit', handleFormSubmit)
+  
